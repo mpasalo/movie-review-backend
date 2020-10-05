e5159768ecaaf59b78615dc862ed75fcf36fd93d
Marc Paul Pasalo <mpasalo@legacyseller.com>
2020-10-05 22:40:03 +0800

    add frontend link

--- public/index.php
+++ public/index.php
@@ -44,7 +44,7 @@ require __DIR__.'/../vendor/autoload.php';
 |
 */
 
-header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Origin: https://epic-clarke-e6440f.netlify.app');
 header('Access-Control-Allow-Methods: GET, POST, PATCH, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Origin, Content-Type, X-Auth-Token');
 
