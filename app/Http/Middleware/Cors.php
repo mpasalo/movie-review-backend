eb46496e1b1dc690897d67d4555ff2ca85dd5c11
Marc Paul Pasalo <mpasalo@legacyseller.com>
2020-10-06 13:09:39 +0800

    update cors file

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -8,8 +8,6 @@ class Cors
 {
     public function handle($request, Closure $next)
     {
-        header("Access-Control-Allow-Origin: *");
-
         $headers = [
             'Access-Control-Allow-Methods' => 'POST, GET, OPTIONS, PUT, DELETE',
             'Access-Control-Allow-Headers' => 'Content-Type, X-Auth-Token, Origin, Authorization',
