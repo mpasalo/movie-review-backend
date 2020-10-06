c085fc7774cc1f5fe478e23413aab462dc95650d
Marc Paul Pasalo <mpasalo@legacyseller.com>
2020-10-06 17:20:19 +0800

    remove return

--- app/Http/Controllers/ReviewController.php
+++ app/Http/Controllers/ReviewController.php
@@ -41,6 +41,6 @@ class ReviewController extends Controller
     public function destroy(Movie $movie)
     {
         $review = Review::find($movie->review->id);
-        return $review->delete();
+        $review->delete();
     }
 }
