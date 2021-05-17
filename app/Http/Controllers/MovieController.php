c914e951fa958e0a9d78696cc125209bde9893a9
Marc_Pasalo <marc.paulpasalo@mymoovz.com>
2021-05-17 17:45:15 +0800

    eager load

--- app/Http/Controllers/MovieController.php
+++ app/Http/Controllers/MovieController.php
@@ -11,7 +11,7 @@ class MovieController extends Controller
 {
     public function index()
     {
-        return Movie::all();
+        return Movie::with('review')->get();
     }
 
     public function filterByReview()
